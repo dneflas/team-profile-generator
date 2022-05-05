94791babc7d6cc91b7dd0133f1ea0d6fb37e42a8
Debbie Neflas <debbieneflas@gmail.com>
2022-05-05 00:18:54 -0700

    Employee test suite

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,33 @@
+const Employee = require('../lib/Employee');
+
+test('create an Employee parent class', () => {
+    const employee = new Employee('Melanie', '1234567', 'melanie@awesome.com');
+    
+    expect(employee.name).toBe('Melanie');
+    expect(employee.id).toBe(expect.any(String));
+    expect(employee.email).toBe(expect.stringContaing('@'));
+});
+
+test("returns employee's name", () => {
+    const employee = new Employee('Melanie', '1234567', 'melanie@awesome.com');
+
+    expect(employee.getName()).toEqual('Melanie');
+});
+
+test ("returns employee's id number", () => {
+    const employee = new Employee('Melanie', '1234567', 'melanie@awesome.com');
+
+    expect(employee.getId()).toEqual('1234567');
+});
+
+test ("returns employee's phone number", () => {
+    const employee = new Employee('Melanie', '1234567', 'melanie@awesome.com');
+
+    expect(employee.getEmail()).toEqual('melanie@awesome.com');
+});
+
+test ("returns employee's role", () => {
+    const employee = new Employee('Melanie', '1234567', 'melanie@awesome.com');
+
+    expect(employee.getRole()).toEqual('Employee');
+});
