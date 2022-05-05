ac9ab2c584efb95828c50a23ba9102dc5705b8a3
Debbie Neflas <debbieneflas@gmail.com>
2022-05-05 16:28:48 -0700

    Engineer class and update to Engineer test

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -3,13 +3,13 @@ const Engineer = require('../lib/Engineer');
 test('create Engineer class', () =>{
     const engineer = new Engineer('Tawnya', '987654', 'tawnya@awesome.com', 'tbeezy');
 
-    expect(engineer.github).toBe(expect.any(String));
+    expect(engineer.github).toEqual(expect.any(String));
 });
 
 test('return github username', () =>{
     const engineer = new Engineer('Tawnya', '987654', 'tawnya@awesome.com', 'tbeezy');
 
-    expect(engineer.getGithub()).toEqual(engineer.githuhb);
+    expect(engineer.getGithub()).toEqual('tbeezy');
 });
 
 test('role overriden to Engineer', () =>{
