6cc3ddfbe67ebb047ce504906559b4262f6df8fb
Debbie Neflas <debbieneflas@gmail.com>
2022-05-13 23:03:42 -0700

    update Manager test and class

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,6 +1,6 @@
 const Manager = require('../lib/Manager');
 
-test("returns manager's office number", () => {
+test("create intern class", () => {
     const manager = new Manager('Amanda', '4567890', 'amanda@awesome.com', '310-555-5555');
 
     expect(manager.name).toEqual('Amanda');
@@ -8,6 +8,11 @@ test("returns manager's office number", () => {
     expect(manager.email).toEqual('amanda@awesome.com');
     expect(manager.officeNumber).toEqual('310-555-5555');
 });
+
+test("returns manager's office number", () => {
+    const manager = new Manager('Amanda', '4567890', 'amanda@awesome.com', '310-555-5555');
+    expect(manager.getOfficeNumber()).toEqual('310-555-5555');
+})
 
 test('role overriden to return Manager', () =>{
     const manager = new Manager('Amanda', '4567890', 'amanda@awesome.com', '310-555-5555');
